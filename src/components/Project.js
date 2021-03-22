a0b06816e68af7d79ff19289b7a074e5caf341c4
JohnWestermeyer <johnwestermeyer@gmail.com>
2021-03-22 14:24:39 -0700

    small update

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,10 +3,9 @@ import React from 'react';
 function Project(props) {
     return(
         <>
-        <a href={props.link}>
+        <a href={props.git}>
             <img src={props.img} alt={props.alt} className="image-fluid float-left mr-3 mb-3"/>
         </a>
-        <a href={props.git}>GitHub</a>
         </>
     )
 
