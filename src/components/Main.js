3e46cd79c3c9dce3f4cf69cf19d9a44a15ec1185
JohnWestermeyer <johnwestermeyer@gmail.com>
2021-03-30 12:43:55 -0700

    updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,10 @@ function Main() {
         <h1 style={{"color":"#0a6751"}}>About Me</h1>
         <hr/>
         <img src={headshot} alt="headshot" className="image-fluid float-left mr-1 pt-1" />
-        <p>Hello, my name is John Westermeyer. I am a front-end web developer and current full-stack web development student. My skills include HTML, CSS, Javascript and jQuery.
+        <p>Hello, my name is John Westermeyer. I am a Front-End Web Developer leveraging a background in retail to build a more intuitive user experience on the web. Known as an innovative problem-solver passionate about developing apps. Strengths in meeting deadlines, creativity and teamwork. Known among staff for a strong work ethic no matter the complexity of the project.
+        </p>
+        <p>
+        Languages: JavaScript, HTML, CSS, SQL, React, NodeJs, JQuery, Bootstrap
         </p>
         <p>
         Located in Long Beach, California.
